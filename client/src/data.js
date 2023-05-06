c450e0a57736683799a67cb906e81cefada2d77f
Anant Dwivedi <anantdwivedi912@gmail.com>
2023-05-06 23:31:05 +0530

    updated api , pushed to github

---
+++ client/src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.kindpng.com/picc/m/546-5465993_fashion-men-png-mens-wear-images-png-transparent.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      // img: "https://i.ibb.co/DG69bQ4/2.png",
+      img: "https://www.kindpng.com/picc/m/4-40863_transparent-men-fashion-png-red-bow-tie-wedding.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2164373/2017/11/15/11510749192975-Roadster-Men-Brown--Black-Regular-Fit-Checked-Casual-Shirt-8071510749192783-1.jpg",
+      title: "SHIRT STYLE!",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://cdn.luxe.digital/media/2020/03/18150849/men-loungewear-style-luxury-home-luxe-digital.jpg",
+      title: "LOUNGEWEAR LOVE",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+      cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.prada.com/content/dam/pradabkg_products/U/UMR/UMR534/11QUF0009/UMR534_11QU_F0009_S_222_SLF.jpg/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.crop.jpg",
+    },
+    {
+      id:2,
+      img:"https://m.media-amazon.com/images/I/41dJQd4+nqL._UX569_.jpg",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://imagescdn.thecollective.in/img/app/product/7/763506-8751787.jpg?w=1600&auto=format",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11807622/2020/4/21/a92e2485-782a-464e-8dd3-3163a23188141587461238631Puma1.jpg",
+    },
+    {
+      id:7,
+      img:"https://www.armani.com/variants/images/17266703523686548/F/w400.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
